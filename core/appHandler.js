d48c30e283836f850b72f0c82306ed32678f9556
Potter <118960067+Potterpurpp@users.noreply.github.com>
2024-07-24 14:10:44 +0700

    Fix high security

--- core/appHandler.js
+++ core/appHandler.js
@@ -245,31 +245,24 @@ module.exports.bulkProductsLegacy = function (req, res) {
 };
 
 module.exports.bulkProducts = function (req, res) {
-  if (req.files.products && req.files.products.mimetype == "text/xml") {
-    let products = libxmljs.parseXmlString(
-      req.files.products.data.toString("utf8"),
-      {
-        noblanks: true,
-        noent: true, // Noncompliant
-        nocdata: true,
-      }
-    );
-    products
-      .root()
-      .childNodes()
-      .forEach((product) => {
-        let newProduct = new db.Product();
-        newProduct.name = product.childNodes()[0].text();
-        newProduct.code = product.childNodes()[1].text();
-        newProduct.tags = product.childNodes()[2].text();
-        newProduct.description = product.childNodes()[3].text();
-        newProduct.save();
-      });
-    res.redirect("/app/products");
-  } else {
-    res.render("app/bulkproducts", {
-      messages: { danger: "Invalid file" },
-      legacy: false,
-    });
-  }
-};
+	if (req.files.products && req.files.products.mimetype == "text/xml") {
+	  let products = libxmljs.parseXmlString(req.files.products.data.toString("utf8"));
+	  products
+		.root()
+		.childNodes()
+		.forEach((product) => {
+		  let newProduct = new db.Product();
+		  newProduct.name = product.childNodes()[0].text();
+		  newProduct.code = product.childNodes()[1].text();
+		  newProduct.tags = product.childNodes()[2].text();
+		  newProduct.description = product.childNodes()[3].text();
+		  newProduct.save();
+		});
+	  res.redirect("/app/products");
+	} else {
+	  res.render("app/bulkproducts", {
+		messages: { danger: "Invalid file" },
+		legacy: false,
+	  });
+	}
+  };
